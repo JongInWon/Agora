cd0f896f1eef6c780387edcb1fc9d7e58181dd35
jongin <jonginwon1203@gmail.com>
2024-06-26 15:10:44 +0900

    Design: "헤더 레이아웃 추가"
    로그인 로그아웃에 쓸 헤더 레이아웃을 추가함.

--- src/main/java/com/agora/agora/security/configs/AuditorAwareImpl.java
+++ src/main/java/com/agora/agora/security/configs/AuditorAwareImpl.java
@@ -1,11 +1,13 @@
 package com.agora.agora.security.configs;
 
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@NonNullApi
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
