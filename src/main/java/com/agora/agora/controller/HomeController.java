e38e5bea104e2a7ba5ee8c2f6c02a853baaffe94
jongin <jonginwon1203@gmail.com>
2024-06-26 17:16:20 +0900

    Feat: "POST 방식 로그아웃 구현"
    POSt 방식으로 로그아웃을 하는 기능을 구현함.

--- src/main/java/com/agora/agora/controller/HomeController.java
+++ src/main/java/com/agora/agora/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.agora.agora.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Agora";
+        return "home";
     }
 
     @GetMapping("/articles")
--- src/main/java/com/agora/agora/security/configs/SecurityConfig.java
+++ src/main/java/com/agora/agora/security/configs/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.agora.agora.security.configs;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +28,24 @@ public class SecurityConfig {
                         .requestMatchers("/", "signup").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                        .logoutSuccessUrl("/")
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .addLogoutHandler(new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                request.getSession().invalidate();
+                                SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+                                SecurityContextHolder.getContextHolderStrategy().clearContext();
+                            }
+                        })
+                        .permitAll()
+                )
                 .authenticationProvider(authenticationProvider)
                 .build();
     }
-
-
 }
